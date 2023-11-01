da9108296bb4680280598b946d8d56858e8278d9
Felipe Soares <felipe.11.11@hotmail.com>
2023-11-01 14:55:52 -0400

    Feat: Create base Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
+import { AppContainer, AppHeader } from '@/styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppContainer>
+      <AppHeader>
+        <Image src={logoImg} alt="" />
+      </AppHeader>
+      <Component {...pageProps} />
+    </AppContainer>
+  )
 }
